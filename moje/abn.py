6278071bc5dcf32a795e063a0728d7517746f125
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2022-11-07 19:00:20 +0100

    L

---
+++ moje/abn.py
@@ -0,0 +1,39 @@
+from math import sqrt
+from time import time
+def jejich(n) -> None:
+    #n: int = int(input("n = "))
+    out: list[str] = []
+    t1: float = time()
+    for a in range(0, n + 1):
+        b = n -2*sqrt(n*a)+a
+        if b.is_integer():
+            out.append(f"\na = {a} b = {b}")
+    t2: float = time()
+    print(*out)
+    print(f"Time: {t2 - t1}")
+
+def moje(n) -> None:
+    #n: int = int(input("n = "))
+    t1: float = time()
+    out_a: list[int] = []
+    for a in range(0, n + 1):
+        if is_square(n*a): # or is_square(4*n*a) <- not needed
+            out_a.append(a)
+    out: list[str] = [f"\na = {a} b = {b}" for a,b in zip(out_a, out_a[::-1])]
+    t2: float = time()
+    print(*out)
+    print(f"Time: {t2 - t1}")
+
+def is_square(apositiveint) -> bool:
+  x: int = apositiveint // 2
+  seen: set[int] = set([x])
+  while x * x != apositiveint:
+    x = (x + (apositiveint // x)) // 2
+    if x in seen: return False
+    seen.add(x)
+  return True
+
+if __name__ == "__main__":
+    n = int(input("n = "))
+    jejich(n)
+    moje(n)
--- moje/temp.py
+++ moje/temp.py
@@ -1,28 +1,5 @@
-import random
-
-
-with open("moje/p/primes.txt", "r") as f:
-    primes: list[int] = [int(f.readline()) for i in range(10)]
-# generate 10 random numbers in list
-flt = [random.randint(2, 1000000) for i in range(10)]
-out: list[int] = []
-m=0
-n=0
-while n == 0:
-    while m == 0:
-        for i in primes:
-            for int in flt:
-                if int % i == 0:
-                    out.append(i)
-                    num //= i
-                    den //= i
-                    m:int = 1
-                    break
-        if m == 1:
-            m = 0
-            continue
-        n = 1
-        break
-
-print(out)
-print(f"{flt} = {num}/{den}")
+x: float = 0.5
+while True:
+    if (x**2).is_integer():
+        print(x)
+    x += 1

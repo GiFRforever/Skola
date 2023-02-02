221459e0328bdd5255083a9770009e221f635d01
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2023-02-02 19:26:41 +0100

    rc. 1

--- moje/autocatalog.py
+++ moje/autocatalog.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, re
 
 
 def decide_where_to_move(path_org: str, výstupní_místa: list[str]) -> int:
@@ -21,14 +21,16 @@ def decide_where_to_move(path_org: str, výstupní_místa: list[str]) -> int:
 
 def rename_folder(path: str) -> tuple[str, str]:
     # rename the folder
-    for root, _, files in os.walk(path):
+    for i, (root, _, files) in enumerate(os.walk(path)):
         for file in files:
-            file_parts: list[str] = file.split(".")
+            file_parts: list[str] = re.split("[. _-]", file)
             if file_parts[-1] in ["mkv", "mp4", "avi", "srt", "idx", "sub"]:
                 file_name: list[str] = []
-                for part in file_parts[:-1]:
+                for i, part in enumerate(file_parts[:-1]):
                     try:
-                        if int(part[:-1]) < 100:  # if it is a resolution or a year
+                        if (
+                            int(part[:-1]) < 100 and i != 0
+                        ):  # if it is a resolution or a year
                             raise Exception
                         break
                     except:
@@ -46,25 +48,30 @@ def rename_folder(path: str) -> tuple[str, str]:
                 )
             else:
                 os.remove(os.path.join(root, file))
-    root_name: list[str] = path.split("\\")[-2].split(".")
-    root_name_new: list[str] = []
-    season: str = ""
-    for part in root_name:
-        if part[0] == "S" and part[1:].isnumeric():
-            season: str = part
-            break
-        root_name_new.append(part)
-    parentdir: str = "\\".join(path.split("\\")[:-2])
-    os.makedirs(
-        os.path.join(parentdir, " ".join(root_name_new)),
-        exist_ok=True,
-    )
-    os.rename(path, parentdir + "\\" + season)
-    shutil.move(
-        parentdir + "\\" + season,
-        os.path.join(parentdir, " ".join(root_name_new)),
-    )
-    return parentdir, " ".join(root_name_new)
+
+    if type_of_media == "s":
+        parentdir: str = "\\".join(path.split("\\")[:-2])
+        root_name: list[str] = path.split("\\")[-2].split(".")
+        root_name_new: list[str] = []
+        season: str = ""
+        for part in root_name:
+            if part[0] == "S" and part[1:].isnumeric():
+                season: str = part
+                break
+            root_name_new.append(part)
+        os.makedirs(
+            os.path.join(parentdir, " ".join(root_name_new)),
+            exist_ok=True,
+        )
+        os.rename(path, parentdir + "\\" + season)
+        shutil.move(
+            parentdir + "\\" + season,
+            os.path.join(parentdir, " ".join(root_name_new)),
+        )
+        return parentdir, " ".join(root_name_new)
+
+    else:
+        return path, ""
 
 
 path_org: str = input("Enter path to original folder: ")
@@ -79,7 +86,12 @@ if path_org[-1] != "\\":
 výstupní_místa_seriály: list[str] = ["T:\\Seriály\\"]
 výstupní_místa_filmy: list[str] = ["I:\\Filmy\\", "J:\\"]
 
-type_of_media: str = input("Enter type of media (serial or movie): ")
+if path_org.split("\\")[1] == "Seriály":
+    type_of_media: str = "s"
+elif path_org.split("\\")[1] == "Filmy":
+    type_of_media: str = "m"
+else:
+    type_of_media: str = input("Enter type of media (serial or movie): ")
 
 if type_of_media[0] == "s":
     path_new: str = výstupní_místa_seriály[
@@ -102,4 +114,9 @@ if type_of_media == "s":
     # windows robocopy
     os.system(
         f'start /wait cmd /c robocopy  "{os.path.join(parent_dir, new_name)}" "{os.path.join(path_new, new_name)}" /S /MOVE'
+    )
+elif type_of_media == "m":
+    parent_dir, _ = rename_folder(path_org)
+    os.system(
+        f'start /wait cmd /c robocopy  "{parent_dir[:-1]}" "{path_new[:-1]}" /MOVE'  # type: ignore
     )

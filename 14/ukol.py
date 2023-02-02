0cee4e6efae6a41b2a69c12fea1140758386fde8
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2023-02-02 17:55:07 +0100

    done?

--- 14/ukol.py
+++ 14/ukol.py
@@ -18,11 +18,8 @@ tělesa: list[str] = [
 
 komolá_tělesa: list[str] = [
     "_zpět_",
-    "komolý kvádr",
-    "komolý hranol",
     "komolý jehlan",
     "komolý kužel",
-    "komolý válec",
 ]
 
 mnohostěny: list[str] = [
@@ -31,6 +28,44 @@ mnohostěny: list[str] = [
     "osmistěn",
     "dvanáctistěn",
     "dvacetistěn",
+]
+
+koule: list[str] = [
+    r"      ((((((///**,,   ",
+    r"   ###%%%%##((//**,,. ",
+    r"  #%%&&&&&%%##(//**,..",
+    r" (#%%&&@@&&%##(//**,...",
+    r"/(##%%%%%%%##((//*,,...",
+    r" /(((#####(((//**,,....",
+    r"  */////////***,,..... ",
+    r"   ,,*****,,,,......   ",
+    r"      ...........      ",
+]
+
+kvádr: list[str] = [
+    r"    @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @",
+    r"  @ @                             @ @",
+    r"@ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @   @",
+    r"@   @                           @   @",
+    r"@   @                           @   @",
+    r"@   @                           @   @",
+    r"@   @                           @   @",
+    r"@   @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ ",
+    r"@ @                             @ @",
+    r"@ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ @ ",
+]
+
+krychle: list[str] = [
+    r"    @ @ @ @ @ @ @ @ @ @",
+    r"  @ @               @ @",
+    r"@ @ @ @ @ @ @ @ @ @   @",
+    r"@   @             @   @",
+    r"@   @             @   @",
+    r"@   @             @   @",
+    r"@   @             @   @",
+    r"@   @ @ @ @ @ @ @ @ @ @ ",
+    r"@ @               @ @",
+    r"@ @ @ @ @ @ @ @ @ @ ",
 ]
 
 # clear = lambda: os.system('cls')
@@ -48,20 +83,18 @@ def vstup_float(txt) -> float:
             clean(1)
             print("\033[93mChybný vstup\033[0m", end="\r")
             time.sleep(1)
-            # print("            ", end="\r")
 
 
-def vstup_stran(txt) -> int:
+def vstup_float_min(txt, min) -> int:
     while True:
         try:
-            if (a := int(input(txt))) > 2:
+            if (a := int(input(txt))) > min:
                 return a
             raise ValueError
         except:
             clean(1)
             print("\033[93mChybný vstup\033[0m", end="\r")
             time.sleep(1)
-            # print("            ", end="\r")
 
 
 def zadejte_strany(
@@ -70,14 +103,14 @@ def zadejte_strany(
     for a in args:
         rozměry[a] = vstup_float(f"Zadejte {a}: ")
     if kolikastranný:
-        rozměry["kolikastranný"] = vstup_stran("Zadejte kolikati je stranný: ")
+        rozměry["kolikastranný"] = vstup_float_min("Zadejte kolikati je stranný: ", 2)
         # počet_stran = rozměry["kolikastranný"]
 
     if počet_stran:
         rozměry["strany"] = [
             x
             for n in range(počet_stran)
-            for x in [vstup_float(f"Zadejte stranu {chr(n+97)}: ")]
+            for x in [vstup_float_min(f"Zadejte stranu {chr(n+97)}: ", 0)]
         ]
     if výška:
         rozměry["výška"] = vstup_float("Zadejte výšku: ")
@@ -90,6 +123,13 @@ class ED:
         self.těleso: str = těleso
         self.oběm: float = 0
         self.povrch: float = 0
+        try:
+            for line in globals()[self.těleso]:
+                print(40 * " " + line)
+            for _ in range(len(globals()[self.těleso])):
+                sys.stdout.write("\033[F")
+        except:
+            pass
         print(f"\n\033[95m{self.těleso.capitalize()}\033[0m\n")
 
     def output(self) -> None:
@@ -100,8 +140,10 @@ class ED:
             f"\033[95mOběm\033[0m   {self.těleso} je: \033[96m{round(self.oběm, 4)}\033[0m"
         )
         input("\nStiskni enter pro pokračování...")
-        clean(1)
-        print("\033[33m"+"─"*30 + "\033[0m")
+        sys.stdout.write("\033[F")
+        print("\033[33m" + "─" * 32 + "\033[0m")
+
+    # obecná tělesa
 
     def krychle(self) -> None:
         zadejte_strany(počet_stran=1)
@@ -181,6 +223,89 @@ class ED:
         self.povrch = 4 * mt.pi**2 * rozměry["poloměr"] * rozměry["vnitřní poloměr"]
         self.output()
 
+    # komolá tělesa
+
+    def komolý_jehlan(self) -> None:
+        zadejte_strany(
+            "délku spodní podstavy", "délku horní podstavy", výška=1, kolikastranný=1
+        )
+        s1 = (rozměry["kolikastranný"] * rozměry["délku spodní podstavy"] ** 2) / (
+            4 * mt.tan(mt.pi / rozměry["kolikastranný"])
+        )
+        s2 = (rozměry["kolikastranný"] * rozměry["délku horní podstavy"] ** 2) / (
+            4 * mt.tan(mt.pi / rozměry["kolikastranný"])
+        )
+        r1 = rozměry["délku spodní podstavy"] / (
+            2 * mt.tan(mt.pi / rozměry["kolikastranný"])
+        )
+        r2 = rozměry["délku horní podstavy"] / (
+            2 * mt.tan(mt.pi / rozměry["kolikastranný"])
+        )
+        c = mt.sqrt((r1 - r2) ** 2 + rozměry["výška"] ** 2)
+        spl = (
+            (rozměry["délku spodní podstavy"] + rozměry["délku horní podstavy"])
+            * c
+            * rozměry["kolikastranný"]
+            / 2
+        )
+        self.oběm = (s1 + mt.sqrt(s1 * s2) + s2) * rozměry["výška"] / 3
+        self.povrch = s1 + s2 + spl
+        self.output()
+
+    def komolý_kužel(self) -> None:
+        zadejte_strany("poloměr spodní podstavy", "poloměr vrchní podstavy", výška=1)
+        self.oběm = (
+            mt.pi
+            * rozměry["výška"]
+            * (
+                rozměry["poloměr spodní podstavy"] ** 2
+                + rozměry["poloměr vrchní podstavy"] ** 2
+                + rozměry["poloměr spodní podstavy"]
+                * rozměry["poloměr vrchní podstavy"]
+            )
+            / 3
+        )
+        self.povrch = mt.pi * (
+            (rozměry["poloměr spodní podstavy"] + rozměry["poloměr vrchní podstavy"])
+            * mt.sqrt(
+                rozměry["poloměr spodní podstavy"] ** 2
+                - 2
+                * rozměry["poloměr spodní podstavy"]
+                * rozměry["poloměr vrchní podstavy"]
+                + rozměry["poloměr vrchní podstavy"] ** 2
+                + rozměry["výška"] ** 2
+            )
+            + rozměry["poloměr spodní podstavy"] ** 2
+            + rozměry["poloměr vrchní podstavy"] ** 2
+        )
+        self.output()
+
+    # platónská tělesa
+
+    def čtyřstěn(self) -> None:
+        zadejte_strany(počet_stran=1)
+        self.oběm = mt.sqrt(2) * rozměry["strany"][0] ** 3 / 12
+        self.povrch = mt.sqrt(3) * rozměry["strany"][0] ** 2
+        self.output()
+
+    def osmistěn(self) -> None:
+        zadejte_strany(počet_stran=1)
+        self.oběm = mt.sqrt(2) * rozměry["strany"][0] ** 3 / 3
+        self.povrch = mt.sqrt(3) * 2 * rozměry["strany"][0] ** 2
+        self.output()
+
+    def dvanáctistěn(self) -> None:
+        zadejte_strany(počet_stran=1)
+        self.oběm = (mt.sqrt(5) * 7 + 15) * rozměry["strany"][0] ** 3 / 4
+        self.povrch = mt.sqrt(25 + 10 * mt.sqrt(5)) * 3 * rozměry["strany"][0] ** 2
+        self.output()
+
+    def dvacetistěn(self) -> None:
+        zadejte_strany(počet_stran=1)
+        self.oběm = (mt.sqrt(5) + 3) * 5 * rozměry["strany"][0] ** 3 / 12
+        self.povrch = mt.sqrt(3) * 5 * rozměry["strany"][0] ** 2
+        self.output()
+
 
 def pravidelné_mnohostěny() -> None:
     clean(len(tělesa) + 4)
@@ -219,7 +344,7 @@ def komolá() -> None:
         return
     elif těleso in komolá_tělesa:
         clean(len(komolá_tělesa) + 4)
-        getattr(globals()["ED"](těleso=těleso), těleso)()
+        getattr(globals()["ED"](těleso=těleso), těleso.replace(" ", "_"))()
     else:
         exit()
 

a7afa99a55cfde16a428526d44a9da64b549e41a
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2023-02-15 17:29:35 +0100

    ale nic

---
+++ moje/mount.bat
@@ -0,0 +1,6 @@
+net use i: \\BAGR\i 2684 /user:Plex /persistent:yes
+net use j: \\BAGR\j 2684 /user:Plex /persistent:yes
+net use t: \\BAGR\t 2684 /user:Plex /persistent:yes
+net use u: \\BAGR\u 2684 /user:Plex /persistent:yes
+net use z: \\BAGR\z 2684 /user:Plex /persistent:yes
+net use y: \\BAGR\c 2684 /user:Plex /persistent:yes
---
+++ moje/qbittorrentbatch.py
@@ -0,0 +1,33 @@
+import sys, os
+
+arguments_set: set[str] = {
+    r"%N",  # Název torrentu
+    r"%L",  # Kategorie
+    r"%G",  # Štítky (oddělené čárkou)
+    r"%F",  # Umístění obsahu (stejné jako zdrojová cesta u vícesouborového torrentu)
+    r"%R",  # Zdrojová cesta (první podadresář torrentu)
+    r"%D",  # Cesta pro uložení
+    r"%C",  # Počet souborů
+    r"%Z",  # Velikost torrentu (v bytech)
+    r"%T",  # Současný tracker
+    r"%I",  # Info hash v1
+    r"%J",  # Info hash v2
+    r"%K",  # Torrent ID
+}
+
+arguments: dict[str, str] = {
+    argument: sys.argv[i] for i, argument in enumerate(arguments_set, start=1)
+}
+
+commands: list[str] = [
+    """curl --form-string "message=%N download complete" "https://qbpushlite.fengmlo.com/api/v1/push/dDFTecZrCJQ4be7wlS9_g7YGbqW5CnmBIzH6X4DZdZLpTdbbqajseJHMPvEw24w5.VwCpJkayfQbRIDgrPIeT-yfKUFSJ0fW4""",
+    # "python3 c:\\qlog\\autocatalog.py %D",
+]
+
+for c in commands:
+    for aold, anew in arguments.items():
+        c: str = c.replace(aold, anew)
+    os.system(c)
+
+with open("C:\\qlog\\qbittorrent.log", "w") as f:
+    f.write(" | ".join([f"{aold}: {anew}" for aold, anew in arguments.items()]))

9d9d0b8f7e41e867a80ef1c01ac1ec7903061a81
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2023-05-15 09:25:32 +0200

    ch r done

--- praxe/chuliovo rande.py
+++ praxe/chuliovo rande.py
@@ -2,15 +2,51 @@ def vstup(txt) -> str:
     while True:
         sequence: str = input(txt)
         for s in sequence:
-            if s not in "wsad":
+            if s not in "wsadWSAD":
                 print("Neplatný postup")
                 continue
         else:
             return sequence
 
 
-def time_check(sequence: str) -> bool:
-    sequence = sequence.lower()
+def time_check(seq: str) -> bool:
+    if len(seq) != 12:
+        print("Špatná délka postupu!")
+        return False
+
+    sequence: str = seq.lower()
+
+    invalid_parts: list[str] = ["ws", "sw", "ad", "da"]
+    for part in invalid_parts:
+        if part in sequence:
+            print("Repeating path!")
+            return False
+
+    # p check
+    sequence_p: str = sequence
+    for double, single in zip(["ww", "ss", "aa", "dd"], ["w", "s", "a", "d"]):
+        while double in sequence_p:
+            sequence_p = sequence_p.replace(double, single)
+    # p_invalid_parts: list[str] = ["was", "saw", "wds", "sdw", "asd", "dsa", "aws", "swd"]
+    p_invalid_parts: list[str] = [
+        "wasd",
+        "sawd",
+        "wdsa",
+        "sdaw",
+        "asdw",
+        "dsaw",
+        "awsd",
+        "swda",
+    ]
+    for part in p_invalid_parts:
+        while part in sequence_p:
+            sequence_p = sequence_p.replace(part, "")
+    else:
+        if (len(sequence_p) % 4) != 0:
+            print("Postup je špatný P-return!")
+            return False
+
+    # return check
     w: int = sequence.count("w")
     s: int = sequence.count("s")
     a: int = sequence.count("a")

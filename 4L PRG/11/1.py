52a599aae0be39cd65f76788cc9c010bf3a076e2
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2023-11-27 10:08:37 +0100

    no změny a něco

--- 4L PRG/11/1.py
+++ 4L PRG/11/1.py
@@ -1,8 +1,8 @@
-from tkinter import *
+import tkinter as tk
 
 
 def Smazat():
-    vstup.delete(0, END)  # smaže vše
+    vstup.delete(0, tk.END)  # smaže vše
 
 
 def Vlozit():
@@ -12,31 +12,30 @@ def Vlozit():
 
 
 def Oznacit():
-    vstup.select_range(0, END)  # označí celý text
+    vstup.select_range(0, tk.END)  # označí celý text
 
 
 def Zobrazit():
     vystup["text"] = vstup.get()  # get()- vrací hodnotu
 
 
-hlavni = Tk()
-vstup = Entry(hlavni, width=20, font="Arial 12", bd=5)  # okraj
-vystup = Label(hlavni, text="výstup", font="Arial 15")
-akce = LabelFrame(
+hlavni = tk.Tk()
+vstup = tk.Entry(hlavni, width=20, font="Arial 12", bd=5)  # okraj
+vystup = tk.Label(hlavni, text="výstup", font="Arial 15")
+akce = tk.LabelFrame(
     hlavni, text="Akce", bd=2, relief="ridge", padx=10, pady=10
 )  # padx a pady jsou zde vnitřní vycpávky
 akce.pack(padx=5, pady=5)
 vystup.pack(padx=5, pady=5)
 vstup.pack(padx=5, pady=5)
 vstup.focus_set()  # kurzor nastavený v Entry
-smazat = Button(akce, text="Smaž", width=10, command=Smazat)
+smazat = tk.Button(akce, text="Smaž", width=10, command=Smazat)
 smazat.pack(padx=5, pady=5)
-vlozit = Button(akce, text="Vlož", width=10, command=Vlozit)
+vlozit = tk.Button(akce, text="Vlož", width=10, command=Vlozit)
 vlozit.pack(padx=5, pady=5)
-oznacit = Button(akce, text="Oznac", width=10, command=Oznacit)
+oznacit = tk.Button(akce, text="Oznac", width=10, command=Oznacit)
 oznacit.pack(padx=5, pady=5)
-zobrazit = Button(akce, text="Zobraz", width=10, command=Zobrazit)
+zobrazit = tk.Button(akce, text="Zobraz", width=10, command=Zobrazit)
 zobrazit.pack(padx=5, pady=5)
-
 
 hlavni.mainloop()
--- 4L PRG/11/2.py
+++ 4L PRG/11/2.py
@@ -1,22 +1,22 @@
-from tkinter import *
+import tkinter as tk
 
 
 def Mocnina():
     x = float(vstup.get())
     vystup["text"] = str(x**2)
-    vstup.select_range(0, END)
+    vstup.select_range(0, tk.END)
 
 
-hlavni = Tk()
-ram = Frame(hlavni, width=100, height=50, relief="sunken", bd=3, padx=10, pady=10)
+hlavni = tk.Tk()
+ram = tk.Frame(hlavni, width=100, height=50, relief="sunken", bd=3, padx=10, pady=10)
 ram.pack(padx=5, pady=5)
-vstup = Entry(ram)
+vstup = tk.Entry(ram)
 vstup.pack(
     side="left", padx=5, pady=5
 )  # side - umístění komponent vedle sebe (zde zleva)
 vstup.focus_set()
-vystup = Label(ram, text="výsledek", width=15)
+vystup = tk.Label(ram, text="výsledek", width=15)
 vystup.pack(side="left", padx=5, pady=5)
-vyp = Button(hlavni, text="Druhá mocnina", width=15, command=Mocnina)
+vyp = tk.Button(hlavni, text="Druhá mocnina", width=15, command=Mocnina)
 vyp.pack(padx=5, pady=5)
 hlavni.mainloop()

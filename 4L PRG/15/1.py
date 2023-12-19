700ac26c3df89770c25b61ce9fb13dbadad1125c
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2023-12-19 13:59:03 +0100

    ach jo

--- 4L PRG/15/1.py
+++ 4L PRG/15/1.py
@@ -1,3 +1,4 @@
+from re import U
 import tkinter as tk
 from random import choice
 
@@ -8,23 +9,24 @@ class App(tk.Frame):
         self.master = master
         self.pack()
 
-        self.names: list[str] = ["kámen", "nůžky", "papír", "tapír", "spock"]
+        self.names: list[str] = ["kámen", "nůžky", "papír", "tapír", "Spock"]
 
         self.bounds: dict[str, tuple[tuple[int, int], tuple[int, int]]] = {
             # nazev: (( x1,  x2), ( y1,  y2)),
-            "kámen": ((315, 425), (360, 445)),
-            "nůžky": ((195, 310), (50, 130)),
+            "kámen": ((315, 425), (355, 445)),
+            "nůžky": ((195, 310), (50, 135)),
             "papír": ((375, 460), (180, 265)),
             "tapír": ((50, 190), (360, 445)),
-            "spock": ((35, 105), (160, 250)),
+            "Spock": ((35, 110), (160, 255)),
         }
 
         self.rules: dict[str, list[tuple[str, str]]] = {
+            # nazev: [(nazev, text), (nazev, text)]
             "kámen": [("nůžky", "tupí"), ("tapíra", "rozdrtí")],
             "nůžky": [("papír", "stříhají"), ("tapírovi", "utnou hlavu")],
-            "papír": [("kámen", "balí"), ("spocka", "usvědčí")],
-            "tapír": [("papír", "sní"), ("spocka", "otráví")],
-            "spock": [("kámen", "vypaří"), ("nůžky", "zničí")],
+            "papír": [("kámen", "balí"), ("Spocka", "usvědčí")],
+            "tapír": [("papír", "sní"), ("Spocka", "otráví")],
+            "Spock": [("kámen", "vypaří"), ("nůžky", "zničí")],
         }
 
         self.hra()
@@ -36,6 +38,36 @@ class App(tk.Frame):
         self.canvas.create_image(False, False, image=self.bg, anchor=tk.NW)
         self.master.bind("<Button-1>", self.click)
         self.master.bind("<Escape>", lambda e: self.master.destroy())
+        self.score = (0, 0)
+        self.score_text = self.canvas.create_text(
+            250,
+            470,
+            text="{}:{}".format(*self.score),
+            font=("Impact", 20),
+            fill="white",
+            anchor="center",
+        )
+
+    def update_score(self, outcome: str) -> None:
+        try:
+            self.canvas.delete(self.score_text)
+        except Exception:
+            pass
+
+        if outcome == "win":
+            self.score = (self.score[0] + 1, self.score[1])
+        elif outcome == "lose":
+            self.score = (self.score[0], self.score[1] + 1)
+        else:
+            return
+        self.score_text = self.canvas.create_text(
+            250,
+            470,
+            text="{}:{}".format(*self.score),
+            font=("Impact", 20),
+            fill="white",
+            anchor="center",
+        )
 
     def oponent(self, volba: str):
         protivolba: str = choice(self.names)
@@ -43,6 +75,37 @@ class App(tk.Frame):
 
         if volba == protivolba:
             print("Remíza!")
+            try:
+                self.canvas.delete(self.green_rect)
+                self.canvas.delete(self.red_rect)
+                self.canvas.delete(self.outcome)
+            except Exception:
+                pass
+
+            self.outcome = self.canvas.create_text(
+                250,
+                30,
+                text="Remíza",
+                anchor="center",
+                font=("Impact", 20),
+                fill="orange",
+            )
+
+            x0, x1 = self.bounds[volba][0]
+            y0, y1 = self.bounds[volba][1]
+            # print(x0, x1, y0, y1)
+
+            # create greeen rectangle
+            self.green_rect = self.canvas.create_rectangle(
+                x0, y0, x1, y1, fill="orange"
+            )
+            self.canvas.tag_lower(self.green_rect)
+
+            x0, x1 = self.bounds[protivolba][0]
+            y0, y1 = self.bounds[protivolba][1]
+            # create red rectangle
+            self.red_rect = self.canvas.create_rectangle(x0, y0, x1, y1, fill="orange")
+            self.canvas.tag_lower(self.red_rect)
             return
 
         for k, v in self.rules.items():
@@ -54,8 +117,20 @@ class App(tk.Frame):
                         try:
                             self.canvas.delete(self.green_rect)
                             self.canvas.delete(self.red_rect)
+                            self.canvas.delete(self.outcome)
                         except Exception:
                             pass
+
+                        self.outcome = self.canvas.create_text(
+                            250,
+                            30,
+                            text=f"Vyhrál jsi! {volba} {i[1]} {i[0]}!",
+                            anchor="center",
+                            font=("Impact", 20),
+                            fill="green",
+                        )
+
+                        self.update_score("win")
 
                         x0, x1 = self.bounds[volba][0]
                         y0, y1 = self.bounds[volba][1]
@@ -83,8 +158,20 @@ class App(tk.Frame):
                         try:
                             self.canvas.delete(self.green_rect)
                             self.canvas.delete(self.red_rect)
+                            self.canvas.delete(self.outcome)
                         except Exception:
                             pass
+
+                        self.outcome = self.canvas.create_text(
+                            250,
+                            30,
+                            text=f"Prohrál jsi! {protivolba} {i[1]} {i[0]}!",
+                            anchor="center",
+                            font=("Impact", 20),
+                            fill="red",
+                        )
+
+                        self.update_score("lose")
 
                         x0, x1 = self.bounds[protivolba][0]
                         y0, y1 = self.bounds[protivolba][1]
@@ -118,6 +205,13 @@ class App(tk.Frame):
                 print(f"TY: {k}!")
                 self.oponent(k)
                 return k
+        else:
+            try:
+                self.canvas.delete(self.green_rect)
+                self.canvas.delete(self.red_rect)
+                self.canvas.delete(self.outcome)
+            except Exception:
+                pass
 
 
 okno = tk.Tk()

0b0faa50015ae82fd0f474353b9f4f2355d05864
GiFRforever <93822902+GiFRforever@users.noreply.github.com>
2024-01-14 22:47:43 +0100

    no nejde to jak chci

--- 4L PRG/16/1.py
+++ 4L PRG/16/1.py
@@ -12,13 +12,30 @@ class App(tk.Frame):
         self.kalkulacka()
 
     def kalkulacka(self) -> None:
-        vcmd = (self.master.register(self.validate), '%P')
+        vcmd = (self.master.register(self.validate), "%W", "%P")
+        # print(vcmd)
 
-        self.num1 = tk.Spinbox(self, from_=-100, to=100, validate="key", validatecommand=vcmd)
-        self.num1.pack(padx=10, pady=1)
+        self.num1 = tk.IntVar(self)
+        self.num1sb = tk.Spinbox(
+            self,
+            from_=-100,
+            to=100,
+            textvariable=self.num1,
+            validate="key",
+            validatecommand=vcmd,
+        )
+        self.num1sb.pack(padx=10, pady=1)
 
-        self.num2 = tk.Spinbox(self, from_=-100, to=100, validate="key", validatecommand=vcmd)
-        self.num2.pack(padx=10, pady=1)
+        self.num2 = tk.IntVar(self)
+        self.num2sb = tk.Spinbox(
+            self,
+            from_=-100,
+            to=100,
+            textvariable=self.num2,
+            validate="key",
+            validatecommand=vcmd,
+        )
+        self.num2sb.pack(padx=10, pady=1)
 
         self.vystup = tk.Label(self, text="0", font="Arial 20")
         self.vystup.pack(padx=10, pady=10)
@@ -40,18 +57,34 @@ class App(tk.Frame):
 
         self.master.bind("<Return>", lambda event: self.vypocet())
 
-    def validate(self, value):
+    def validate(self, caller, value) -> bool:
         try:
-            if value == "":
+            if value in ["", "-"]:  # Allow empty entry and minus sign
                 return True
+
+            # print(value)
             # Attempt to convert the entered value to an integer
             int_value = int(value)
-
             # Check if the integer value is within the specified range
-            return -100 <= int_value <= 100 # tady to zprav
+            return -100 <= int_value <= 100
+
+            # if caller == ".!app.!spinbox":
+            #     target: tk.IntVar = self.num1
+            # elif caller == ".!app.!spinbox2":
+            #     target: tk.IntVar = self.num2
+            # else:
+            #     raise Exception("Unknown widget")
+            # if int_value > 100:
+            #     target.set(100)
+            # elif int_value < -100:
+            #     target.set(-100)
+            # else:
+            #     return True
+            # return False
         except ValueError:
             # If the conversion to integer fails, it's not a valid number
             return False
+        return False
 
     def vypocet(self) -> None:
         try:
